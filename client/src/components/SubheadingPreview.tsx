e83db899bbd3d1ae3c745e422cc97da690869d59
BlitzJB <blitz04.dev@gmail.com>
2023-09-05 23:26:22 +0530

    partial: spacing between preview and selector

--- client/src/components/SubheadingPreview.tsx
+++ client/src/components/SubheadingPreview.tsx
@@ -6,7 +6,7 @@ export const SubheadingPreview = ({ content }: { content: string }) => {
     return (
         <ReactMarkdown
             children={content}
-            className={`prose min-w-[70%] max-h-[90vh] overflow-scroll border border-neutral-600 rounded-sm px-6 py-4 ${content === "Generating..." ? "flex items-center justify-center h-full w-full" : ""}`}
+            className={`prose min-w-[70%] md:mt-0 mt-4 max-h-[90vh] overflow-scroll border border-neutral-600 rounded-sm px-6 py-4 ${content === "Generating..." ? "flex items-center justify-center h-full w-full" : ""}`}
             components={{
                 code({node, inline, className, children, ...props}) {
                     const match = /language-(\w+)/.exec(className || '')
